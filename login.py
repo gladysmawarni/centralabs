7334f12518f261a7d1ed73b24b88b817f8ebc971
Gladys Mawarni <gladysmawarni@gmail.com>
2022-10-15 17:45:12 +0100

    using firestore db

---
+++ login.py
@@ -0,0 +1,77 @@
+# import streamlit 
+import streamlit as st
+from google.cloud import firestore
+from google.oauth2 import service_account
+import json
+
+
+## ----- functions -----
+def login():
+    ## dev
+    # db = firestore.Client.from_service_account_json("users/firestore-key.json")
+
+    ## prod
+    key_dict = json.loads(st.secrets["textkey"])
+    creds = service_account.Credentials.from_service_account_info(key_dict)
+    db = firestore.Client(credentials=creds, project="cerntralabs99")
+
+    users = db.collection('registered')
+
+    emails = [user.to_dict()['email'] for user in users.stream()]
+
+
+    with st.form('login_form'):
+        st.subheader('Login')
+        email = st.text_input('Email')
+        password = st.text_input('Password', type= 'password')
+        submit = st.form_submit_button('Login')
+
+    if (not email) | (not password):
+        st.warning('Please fill in your information')
+    else:
+        if email not in emails:
+            st.error('User not registered')
+        else:
+            logged_user = users.document(email).get().to_dict()
+            if password != logged_user['password']:
+                st.error('Password incorrect')
+            else:
+                st.success('Logged in!')
+
+                ## session state
+                st.session_state.name = logged_user['name']
+                st.session_state.authenticated = True
+                
+                return True
+                
+    
+def pageOne():
+    st.write(f'Welcome *{st.session_state.name}*')
+    st.write("Seems like you've logged in!")
+
+def pageTwo():
+    st.write("second page")
+
+
+## ----- app -----
+selection = None
+
+# if user not logged in 
+if "navigation" not in st.session_state:
+    st.session_state.navigation = ["login"]
+    selection = st.session_state.navigation
+
+if "authenticated" not in st.session_state:
+    selection = st.sidebar.radio(label="pages", options=st.session_state.navigation)
+    if login():
+        st.session_state.navigation = ["pageone", "pagetwo"]
+
+# if user logged in
+if "authenticated" in st.session_state and st.session_state['authenticated'] == True:
+    selection = st.sidebar.radio(label="pages", options=st.session_state.navigation)
+
+if selection == "pageone":
+    pageOne()
+
+if selection == "pagetwo":
+    pageTwo()
---
+++ pages/register.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from google.cloud import firestore
+from google.oauth2 import service_account
+import json
+
+
+def register():
+    ## dev
+    # db = firestore.Client.from_service_account_json("users/firestore-key.json")
+
+    ## prod
+    key_dict = json.loads(st.secrets["textkey"])
+    creds = service_account.Credentials.from_service_account_info(key_dict)
+    db = firestore.Client(credentials=creds, project="cerntralabs99")
+
+    preauth = db.collection('preauthorized')
+
+
+    emails = [doc.to_dict()['email'] for doc in preauth.stream()]
+
+    with st.form('register_form'):
+        st.subheader('Registration')
+        email = st.text_input('Email')
+        username = st.text_input('Username').lower()
+        name = st.text_input('Name')
+        password = st.text_input('Password', type= 'password')
+        password2 = st.text_input('Re-type Password', type= 'password')
+        submit = st.form_submit_button('Register')
+
+    if (not email) | (not username) | (not name) | (not password):
+        st.warning('Please fill in your information')
+    else:
+        if email not in emails:
+            st.error('User not authorized')
+        elif password != password2:
+            st.error('Password does not match')
+        else:
+            user_ref = db.collection('registered').document(email)
+            user_ref.set(
+                {
+                    "email" : email,
+                    "username" : username,
+                    "name" : name,
+                    "password" : password
+                }
+            )
+            ## TODO : delete registered preauthorized email
+            st.success('Successfully registered :)')
+
+register()
